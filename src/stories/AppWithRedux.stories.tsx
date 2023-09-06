8bfba45c65a57a6d5ac43e3dcc99acec0271252a
Alexander Korobov <dktwho@mail.ru>
2023-09-06 19:41:44 +0300

    clear unused imports

--- src/stories/AppWithRedux.stories.tsx
+++ src/stories/AppWithRedux.stories.tsx
@@ -1,8 +1,6 @@
 import type {Meta, StoryObj} from '@storybook/react';
 import React from "react";
 import AppWithRedux from "../AppWithRedux";
-import {Provider} from "react-redux";
-import {store} from "../state/store";
 import {ReduxStoreProviderDecorator} from "./decorators/ReduxStoreProviderDecorator";
 
 
